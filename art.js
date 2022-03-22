4e58480d9f67494b645848a3ed086c6f78c2162c
Peter Story <peter.garth.story@gmail.com>
2022-03-22 12:40:42 -0400

    Add data for Macintosh art

--- art.js
+++ art.js
@@ -7,4 +7,8 @@ const ART = [
    "filename": "earth.txt",
    "credit_text": "jgs",
    "credit_url": "https://www.asciiart.eu/space/planets"},
+  {"title": "Macintosh",
+   "filename": "mac.txt",
+   "credit_text": "Unknown Artist",
+   "credit_url": "https://www.asciiart.eu/computers/apple"},
 ];
