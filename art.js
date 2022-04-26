7bbe8e7ed8c3c8b58beed6d732f142cf6e1e0c26
annreillyzimmerman <104455805+annreillyzimmerman@users.noreply.github.com>
2022-04-26 13:39:54 -0400

    Add monkey-eating-banana to art.js

--- art.js
+++ art.js
@@ -7,4 +7,8 @@ const ART = [
    "filename": "earth.txt",
    "credit_text": "jgs",
    "credit_url": "https://www.asciiart.eu/space/planets"},
-];
+  {"title": "Monkey w/ Banana",
+   "filename": "monkey-eating-banana.txt",
+   "credit_text": "jgs",
+   "credit_url": "https://www.asciiart.eu/food-and-drinks/bananas"},
+  ];
