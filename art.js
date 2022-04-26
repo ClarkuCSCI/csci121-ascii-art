f68f319e4671fad045477237c1acbf396bac3ab1
MPatClark <92949759+MPatClark@users.noreply.github.com>
2022-04-26 13:44:35 -0400

    Update art.js

--- art.js
+++ art.js
@@ -7,4 +7,8 @@ const ART = [
    "filename": "earth.txt",
    "credit_text": "jgs",
    "credit_url": "https://www.asciiart.eu/space/planets"},
+  {"title": "Pez",
+   "filename": "Pez.txt",
+   "credit_text": "pru",
+   "credit_url": "https://www.asciiart.eu/toys/pez"},
 ];
