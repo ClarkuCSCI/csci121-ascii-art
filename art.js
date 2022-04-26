8317ed25524239de77b5f5ef7eda1c0b1dd466fb
phoenixp123 <68199764+phoenixp123@users.noreply.github.com>
2022-04-26 13:42:03 -0400

    update art.js with new image

--- art.js
+++ art.js
@@ -7,4 +7,8 @@ const ART = [
    "filename": "earth.txt",
    "credit_text": "jgs",
    "credit_url": "https://www.asciiart.eu/space/planets"},
+  {"title": "WhipNaeNae",
+   "filename": "whip.txt",
+   "credit_text": "pbp",
+   "credit_url": "https://copypastatext.com/whip-nae-nae/"},
 ];
