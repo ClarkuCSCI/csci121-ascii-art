702dcc7122477571f1586a19c9af21ad88205d10
HZ2024 <78070399+HZ2024@users.noreply.github.com>
2022-04-26 13:45:02 -0400

    Update art.js

--- art.js
+++ art.js
@@ -7,4 +7,8 @@ const ART = [
    "filename": "earth.txt",
    "credit_text": "jgs",
    "credit_url": "https://www.asciiart.eu/space/planets"},
+    {"title": "Genie",
+   "filename": "Genie.txt",
+   "credit_text": "https://www.asciiart.eu/",
+   "credit_url": "https://www.asciiart.eu/movies/aladdin"},
 ];
