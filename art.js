e57220e770aa2e222aeb0b17093fb911106bf613
rkutyna <67471832+rkutyna@users.noreply.github.com>
2022-04-26 13:38:38 -0400

    Update art.js
    Add flowers details to art.js

--- art.js
+++ art.js
@@ -7,4 +7,8 @@ const ART = [
    "filename": "earth.txt",
    "credit_text": "jgs",
    "credit_url": "https://www.asciiart.eu/space/planets"},
+  {"title": "Flowers",
+   "filename": "flowers.txt",
+   "credit_text": "Joan G. Stark",
+   "credit_url": "https://www.asciiart.eu/plants/flowers"},
 ];
