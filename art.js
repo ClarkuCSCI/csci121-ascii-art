3ea07bd1499505e0d5c01242efb7fb0518cba61a
Elan <104460870+EShalita@users.noreply.github.com>
2022-04-26 15:42:20 -0400

    Adds details about monkey-art to art.js

--- art.js
+++ art.js
@@ -7,4 +7,8 @@ const ART = [
    "filename": "earth.txt",
    "credit_text": "jgs",
    "credit_url": "https://www.asciiart.eu/space/planets"},
+  {"title": "Monkey",
+  "filename": "monkey.txt",
+  "credit_text": "jgs"
+  "credit_url": "https://www.asciiart.eu/animals/monkeys"},
 ];
