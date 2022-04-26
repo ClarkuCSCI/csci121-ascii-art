0592baa143936d90a7a8f2c8870a8633a4c7428d
Jackson-Chace <104456111+Jackson-Chace@users.noreply.github.com>
2022-04-26 13:47:53 -0400

    Update art.js w/ escher-fountain.txt
    Adds M. C. Escher fountain ASCII art

--- art.js
+++ art.js
@@ -3,8 +3,14 @@ const ART = [
    "filename": "owl.txt",
    "credit_text": "Donovan Bake",
    "credit_url": "https://www.asciiart.eu/animals/birds-land"},
+  
   {"title": "Earth",
    "filename": "earth.txt",
    "credit_text": "jgs",
    "credit_url": "https://www.asciiart.eu/space/planets"},
+  
+  {"title": "Escher Fountain",
+   "filename": "escher-fountain.txt",
+   "credit-text": "Veronica Karlsson",
+   "credit_url": "https://www.asciiart.eu/art-and-design/escher"},
 ];
