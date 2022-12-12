7d2c6506896c592157ee842dd358167b4f44449c
Minh-D-Ngo <120048114+Minh-D-Ngo@users.noreply.github.com>
2022-12-11 21:31:41 -0500

    Update art.js

--- art.js
+++ art.js
@@ -15,4 +15,8 @@ const ART = [
    "filename" : "deer.txt",
    "credit_text" : "",
    "credit_url": "https://www.asciiart.eu/animals/deer"},
+  {"title":"Squirtle",
+   "filename": "squirtle.txt",
+   "credit_text": "hm"
+   "credit_url": "https://www.asciiart.eu/video-games/pokemon"},
 ];
