8471b21abe4327115209ebec12d9d2a9218d47f7
Peter Story <peter.garth.story@gmail.com>
2023-04-27 14:05:55 -0400

    Add dragon art

--- art.js
+++ art.js
@@ -7,4 +7,8 @@ const ART = [
    "filename": "earth.txt",
    "credit_text": "jgs",
    "credit_url": "https://www.asciiart.eu/space/planets"},
+  {"title": "Dragon",
+   "filename": "dragon.txt",
+   "credit_text": "Gunnar Z.",
+   "credit_url": "https://www.asciiart.eu/mythology/dragons"},
 ];
