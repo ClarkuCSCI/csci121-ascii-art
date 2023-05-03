ae454d67790555e2b834cdf120e6179ce5a00151
IsaacTomeho <119877312+IsaacTomeho@users.noreply.github.com>
2023-05-03 13:06:11 -0400

    Add valkyrie details to art.js

--- art.js
+++ art.js
@@ -7,4 +7,8 @@ const ART = [
    "filename": "earth.txt",
    "credit_text": "jgs",
    "credit_url": "https://www.asciiart.eu/space/planets"},
+  {"title": "Valkyrie",
+   "filename": "valkyrie.txt",
+   "credit_text": "VLK",
+   "credit_url": "https://www.asciiart.eu/mythology/gryphon"},
 ];
